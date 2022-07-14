35e203eda5eda9641facf9b91b1188f6fa5054e7
LukeGiles3 <luke.giles333@gmail.com>
2022-07-14 17:00:25 -0600

    stuff needed for FCC tests

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
 const functions = require("firebase-functions");
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = 3000;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
+
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${port}`)
+})
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
